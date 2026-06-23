e1aaa69dfee87de53ada17c94d7fae49bd9dbf6d
YashAgarwal1201 <legoyashx@gmail.com>
2026-06-23 20:44:24 +0530

    chore: removed angular remote app

--- angular-remoteapp/src/main.ts
+++
@@ -1,7 +0,0 @@
-import 'zone.js';
-import('./bootstrap')
-  .then((m) => {
-    const container = document.querySelector('app-root');
-    if (container) m.mount(container as HTMLElement);
-  })
-  .catch((err) => console.error(err));
